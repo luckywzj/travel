f79bcaa266769dfe026ff3cc26b5872854325f40
wangzhenjiang <zhenjiang.wang@risksyun.com>
2019-07-31 14:17:41 +0800

    path 更改

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -9,44 +9,55 @@
     </div>
     <div class="header-right">
       <span class="iconfont arrow-icon">&#xe6aa;</span>
-          修改
+      修改
     </div>
   </div>
 </template>
 <script>
 export default {
-  name:"HomeHeader"
+  name: "HomeHeader"
 };
 </script>
 <style lang="stylus" scoped>
-// @import '../../../assets/styles/varibles.styl'
-  @import '~styles/varibles.styl'
-  .header
-    display :flex
-    line-height:.86rem
-    background :$bgColor
-    color :#fff
-    .header-left
-      width:.64rem
-      float: left
-      .back-icon
-        text-align :center
-        font-size :.4rem
-    .header-input
-      flex:1
-      height :0.64rem
-      line-height .64rem
-      margin-top :.12rem
-      margin-left :.2rem
-      padding-left:.15rem
-      background-color:#fff
-      border-radius .1rem
-      color:#ccc
-    .header-right
-      width :1.24rem
-      float:right
-      text-align :center
-      .arrow-icon
-        margin-left :-.04rem
-        font-size .24rem
+@import '~styles/varibles.styl';
+
+.header {
+  display: flex;
+  line-height: 0.86rem;
+  background: $bgColor;
+  color: #fff;
+
+  .header-left {
+    width: 0.64rem;
+    float: left;
+
+    .back-icon {
+      text-align: center;
+      font-size: 0.4rem;
+    }
+  }
+
+  .header-input {
+    flex: 1;
+    height: 0.64rem;
+    line-height: 0.64rem;
+    margin-top: 0.12rem;
+    margin-left: 0.2rem;
+    padding-left: 0.15rem;
+    background-color: #fff;
+    border-radius: 0.1rem;
+    color: #ccc;
+  }
+
+  .header-right {
+    width: 1.24rem;
+    float: right;
+    text-align: center;
+
+    .arrow-icon {
+      margin-left: -0.04rem;
+      font-size: 0.24rem;
+    }
+  }
+}
 </style>
--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -2,18 +2,10 @@
   <div class="wrapper">
     <swiper :options="swiperOption">
       <swiper-slide v-for="item of swiperList" :key="item.id">
-        <img
-          class="swiper-img"
-          :src="item.imgUrl"
-          alt=""
-        >
+        <img class="swiper-img" :src="item.imgUrl" alt />
       </swiper-slide>
-      <div
-        class="swiper-pagination"
-        slot="pagination"
-      ></div>
+      <div class="swiper-pagination" slot="pagination"></div>
     </swiper>
-
   </div>
 </template>
 <script>
@@ -22,19 +14,22 @@ export default {
   data() {
     return {
       swiperOption: {
-       pagination: {
-            el: '.swiper-pagination'
-       },
-       loop:true,
-       autoplay:true
+        pagination: {
+          el: ".swiper-pagination"
+        },
+        loop: true,
+        autoplay: true
       },
-      swiperList:[
+      swiperList: [
         {
-          id:"001",
-          imgUrl:"http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/e1e587f554e5a8372be44a4c6b73a5cf.jpg_750x200_5732f03f.jpg"
-        },{
-          id:"002",
-          imgUrl:"http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/9f39c398e63315d7375efd08028f040e.jpg_750x200_4f536dd8.jpg"
+          id: "001",
+          imgUrl:
+            "http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/e1e587f554e5a8372be44a4c6b73a5cf.jpg_750x200_5732f03f.jpg"
+        },
+        {
+          id: "002",
+          imgUrl:
+            "http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/9f39c398e63315d7375efd08028f040e.jpg_750x200_4f536dd8.jpg"
         }
       ]
     };
@@ -42,15 +37,19 @@ export default {
 };
 </script>
 <style lang="stylus" scoped>
-.wrapper >>> .swiper-pagination-bullet-active
-    background-color #fff !important
-  .wrapper
-    overflow hidden
-    background-color #ddd
-    height 0
-    width 100%
-    padding-bottom 26.7%
-  .swiper-img
-    width 100%
+.wrapper >>> .swiper-pagination-bullet-active {
+  background-color: #fff !important;
+}
 
+.wrapper {
+  overflow: hidden;
+  background-color: #ddd;
+  height: 0;
+  width: 100%;
+  padding-bottom: 26.7%;
+}
+
+.swiper-img {
+  width: 100%;
+}
 </style>
